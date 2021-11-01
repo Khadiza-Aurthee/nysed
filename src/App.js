595c95b2438bd103b558dcf22bc5430ca2490a1c
Khadiza-Aurthee <90924586+Khadiza-Aurthee@users.noreply.github.com>
2021-11-01 12:30:01 -0400

    My Work reeeee

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import Navbar from './components/Navbar.js';
+import React from "react";
+import Navbar from "./components/Navbar.js";
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
-import './App.css';
-
+import SchoolCard from "./components/SchoolCard.js";
+import data from "./nycSchoolEnrollmentData.js";
+import "./App.css";
+console.log();
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="container">
         <div className="row">
-
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
+          <SchoolCard school={data[0]} />
         </div>
       </div>
     </div>
